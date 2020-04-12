b52c29d1f0f00adc3d45b95582ea0176ecf3652c
ioana Loop <61128884+ioana-loop@users.noreply.github.com>
2020-04-12 00:17:14 -0400

    Add files via upload

---
+++ random.m
@@ -0,0 +1,91 @@
+rng(0,'twister');
+%Sample Data, Randomized
+
+% Finger 0 = Thumb ----------------------------
+% Finger 0(x)
+mf0x = 77.8;
+sf0x = 1;
+f0x = sf0x.*randn(90,1) + mf0x;
+statsf0x = [mean(f0x) std(f0x) var(f0x)];
+
+% Finger 0(y)
+mf0y = 77.8;
+sf0y = 1;
+f0y = sf0y.*randn(90,1) + mf0y;
+statsf0y = [mean(f0y) std(f0y) var(f0y)];
+
+% Finger 0(z)
+mf0z = 
+sf01 = 
+f0z = sf01.*randn(90,1) + mf0z;
+statsf0z = [mean(f0z) std(f0z) var(f0z)];
+
+% Finger 1(x) - Index------------------------
+% Finger 0(x)
+mf1x = 77.8;
+sf1x = 1;
+f1x = sf1x.*randn(90,1) + mf1x;
+statsf1x = [mean(f1x) std(f1x) var(f1x)];
+
+mf1y = 77.8;
+sf1y = 1;
+f1y = sf1y.*randn(90,1) + mf1y;
+statsf1y = [mean(f1y) std(f1y) var(f1y)];
+
+mf1z = 77.8;
+sf1z = 1;
+f1z = sf1z.*randn(90,1) + mf1z;
+statsf1z = [mean(f1z) std(f1z) var(f1z)];
+
+% Finger 2 - Middle--------------------------
+% Finger 2(x)
+mf2x = 77.8;
+sf2x = 1;
+f2x = sf2x.*randn(90,1) + mf2x;
+statsf2x = [mean(f2x) std(f2x) var(f2x)];
+
+mf2y = 77.8;
+sf2y = 1;
+f2y = sf2y.*randn(90,1) + mf2y;
+statsf2y = [mean(f2y) std(f2y) var(f2y)];
+
+mf2z = 77.8;
+sf2z = 1;
+f2z = sf2z.*randn(90,1) + mf2z;
+statsf2z = [mean(f2z) std(f2z) var(f2z)];
+
+% Finger 3 - Ring ----------------------------
+% Finger 3(x)
+
+mf3x = 77.8;
+sf3x = 1;
+f3x = sf3x.*randn(90,1) + mf3x;
+statsf3x = [mean(f3x) std(f3x) var(f3x)];
+
+mf3y = 77.8;
+sf3y = 1;
+f3y = sf3y.*randn(90,1) + mf3y;
+statsf3y = [mean(f3y) std(f3y) var(f3y)];
+
+mf3z = 77.8;
+sf3z = 1;
+f3z = sf3z.*randn(90,1) + mf3z;
+statsf3z = [mean(f3z) std(f3z) var(f3z)];
+
+% Finger 4 - Pinky ----------------------------
+% Finger 4(x)
+
+mf4x = 77.8;
+sf4x = 1;
+f4x = sf4x.*randn(90,1) + mf4x;
+statsf4x = [mean(f4x) std(f4x) var(f4x)];
+
+mf4y = 77.8;
+sf4y = 1;
+f4y = sf4y.*randn(90,1) + mf4y;
+statsf4y = [mean(f4y) std(f4y) var(f4y)];
+
+mf4z = 77.8;
+sf4z = 1;
+f4z = sf4z.*randn(90,1) + mf4z;
+statsf4z = [mean(f4z) std(f4z) var(f4z)];
