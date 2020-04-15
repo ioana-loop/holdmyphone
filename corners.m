a718dc03eabdca2019b7544fa5c2800fc7cf8dbf
ioana Loop <61128884+ioana-loop@users.noreply.github.com>
2020-04-15 14:35:06 -0400

    Add files via upload

---
+++ corners.m
@@ -0,0 +1,76 @@
+
+I = imread('R-hand T-down.PNG')
+imshow(I)
+figure;
+imcontour(I,3)
+imshow(I)
+imgradient(I)
+I=rgb2grey(RGB);
+RGB=imread('R-hand T-down.png')
+I=rgb2gray(RGB)
+
+imshow(I)
+
+points = detectBRISKFeatures(I)
+imshow(I); hold on;
+plot(points.selectStrongest(20));
+clf
+heatmap(spreads_small);
+plot(x,y)
+plot(X,Y)
+plot(Nan, NaN)
+plot(nan, NaN)
+
+scatterhistogram(coordinates.X,coordinates.y);
+scatterhistogram(coordinates.X,coordinates.y);
+ribbon(coordinates.X,coordinates.y)
+plot(coordinates.X,coordinates.y)
+c=coordinates1
+ribbon(c.x, c.y. c.z)
+ribbon(c.X, c.y. c.z)
+parallelplot(coordinates1);
+stackedplot(coordinates1);
+scatter3(x,y,z)
+scatter3(X,y,z)
+x=c(x)
+x=c.x
+
+
+c = h.data;
+scatter3(a,b,c,'filled')
+xlabel('x axis')
+ylabel('y axis')
+zlabel('z axis')
+view(-7,7)
+
+points = BRISKPoints(location)
+points = BRISKPoints(Location)
+points = detectBRISKFeatures(I)
+plot(points.selectStrongest(20));
+J = imread('Marked_Green.PNG')
+RGB2=imread('Marked_Green.png')
+J=rgb2gray(RGB2)
+points = detectBRISKFeatures(J)
+imshow(J(
+imshow(J)
+points = detectBRISKFeatures(J)
+plot(points.selectStrongest(20));
+imshow(J)
+imcontour(J,3)
+%-- 4/8/20, 12:37 AM --%
+insertmarker('Marked_Green.png')
+insertMarker('Marked_Green.png')
+corners = detectHarrisFeatures(I)
+corners = detectHarrisFeatures(i)
+I = imread('R-hand T-down.PNG')
+imshow(I)
+corners = detectHarrisFeatures(I)
+RGB=imread('R-hand T-down.png')
+
+I=rgb2gray(RGB);
+corners = detectHarrisFeatures(I)
+[features, valid_corners] = extractFeatures(I, corners);
+figure; imshow(I); hold on
+plot(valid_corners);
+figure; imshow(I); hold on
+plot(valid_corners);
